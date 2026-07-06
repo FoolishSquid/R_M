1ab2fe82f386bde3c639902f4ea1169bdc4d7aa9
HandsomeSquid <debarkamondaldpsnt@gmail.com>
2026-07-07 02:53:12 +0530

    Delete .vscode directory

--- .vscode/BuzzerCodeEG.py
+++
@@ -1,7 +0,0 @@
-from gpiozero import Buzzer
-from time import sleep
-
-Buzzer = Buzzer(17)  # Connect the buzzer to GPIO pin 17
-Buzzer.on()
-sleep(1)
-Buzzer.off()
--- .vscode/LedCodeEg.py
+++
@@ -1,7 +0,0 @@
-from gpiozero import LED
-from time import sleep
-
-led = LED(17)  # Connect the LED to GPIO pin 17
-led.on()  # Turn on the LED
-sleep(1)  # Keep the LED on for 1 second
-led.off()  # Turn off the LED
